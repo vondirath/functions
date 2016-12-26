e98e6dfb43f93f4e9cd2838379b60edfd3ceee8d
Thomas Lopez <vondirath@gmail.com>
2016-12-26 13:18:47 -0700

    chore pep8 comments
    added comments to methods and pep8

--- testquestion.py
+++ testquestion.py
@@ -1,3 +1,4 @@
+# empty template
 list_content = """
 <ul>
 
@@ -5,21 +6,30 @@ list_content = """
 </ul>
 """
 
+# list while providing a simple edge case that is NOT accomidated for.
 list = ["<h1>a", "bunch", "of", "list", "items"]
 
 
 def format_list(list):
+    """
+    Formats the list argument by adding list tags tabs and a new line.
+    """
     output = ''
     for x in range(len(list)):
-        
         output += ('\t' + '<li>' + list[x] + '</li>' + '\n')
     return output
 
+
 def open_page(list):
+    """
+        this creates the page by writing a html file named 'index.html'
+        calling the format_list method taking a list argument and
+        inserting it into the lost_content template.
+        file is then written to and closed.
+    """
     output_file = open('index.html', 'w')
     content = list_content.format(list_item=format_list(list))
     output_file.write(content)
     output_file.close()
 
-# call
 open_page(list)
--- testquestionplus.py
+++ testquestionplus.py
@@ -1,5 +1,6 @@
-import re 
+import re
 
+# making an empty template
 list_content = """
 <ul>
 
@@ -7,22 +8,40 @@ list_content = """
 </ul>
 """
 
-list = ["<h1>  a", "<h1>bun ch", "of<h1>", " list items spaces?", "/ul>ite ms<script>"]
-
+# this is a list accomidating for a good amount of edge cases
+# for html and python.
+list = ["this", "<h1>is</h1>", "a   ", "list {list_content}(open_page() } \|"]
 
 
 def format_list(list):
+    """
+        formats a list by using python regular expression substitution.
+        this will autoescape most non-alphanumeric characters replaced
+        by empty string all numbers allowed all alphabetical upper and
+        lower case and spaces. output is saved to a empty string as the
+        list is iterated through using the regex as it iterates. tab
+        character is added with html tags and a new line tag so it
+        renders an easy to read format.
+
+    """
     output = ''
     for x in range(len(list)):
         new = re.sub('[^0-9a-zA-Z-\s]+', '', list[x])
         output += ('\t' + '<li>' + new + '</li>' + '\n')
     return output
 
+
 def open_page(list):
+    """
+        this creates the page by writing a html file named 'index.html'
+        calling the format_list method taking a list argument and
+        inserting it into the lost_content template.
+        file is then written to and closed.
+    """
     output_file = open('index.html', 'w')
     content = list_content.format(list_item=format_list(list))
     output_file.write(content)
     output_file.close()
 
+# calls open_page method with hardcoded list as argument
 open_page(list)
-
