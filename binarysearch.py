5b0064aa31ff88468d5b4e8ec9208de6a0675e5b
Thomas Lopez <vondirath@gmail.com>
2016-12-30 12:54:48 -0700

    bug fixed syntax error
    fixed syntax error

--- binarysearch.py
+++ binarysearch.py
@@ -6,11 +6,11 @@ def binary_search(input_array, value):
     high = len(input_array) - 1
     while (low<=high):
         mid = (low + high) / 2
-        if (input_array(mid) == value):
+        if (input_array[mid] == value):
             return mid
         elif (input_array[mid] < value):
             low = mid + 1
-        else: high = mid + 1
+        else: high = mid - 1
     return -1
 
 
