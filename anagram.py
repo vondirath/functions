a1295ddd928c763cc131389d4d490430a78ac70d
Thomas Lopez <vondirath@gmail.com>
2017-01-06 13:07:36 -0700

    docs upload anagram

--- anagram.py
+++ anagram.py
@@ -1,5 +1,27 @@
-string1 ='AdnBndAndBdaBn'
-string2 = 'dAn'
+# brute force
 
-string3 = 'AbrAcadAbRa'
-string4 = 'cAda'
+def get_value(s):
+    value = []
+    for i in s:
+        value.append(ord(i))
+    return sum(value)
+
+
+def isanagram(s, t):
+    lent = len(t)
+    anagram = get_value(t)
+
+    for i in range(0, lent + 1):
+        if get_value(s[i: i + lent]) == anagram:
+            return True
+    return False
+
+
+
+# test cases
+# true
+print isanagram("banana", 'nana')
+# false
+print isanagram("longstring", "yell")
+# false
+print isanagram("1234", "seven")
