d048457cf2d2db3ac586a17e5e6aafa947ec1581
Thomas Lopez <vondirath@gmail.com>
2017-01-11 12:29:01 -0700

    feat started brainteasers
    started brainteaser answers in python

---
+++ brainteasers/multiaddsub.py
@@ -0,0 +1,7 @@
+# brainteaser if you see it
+def mathproblem(x, y):
+    answer = []
+    one = (x * y).append(answer)
+    two = x + y.append(answer)
+    three = (one - two).append(answer)
+    return answer
