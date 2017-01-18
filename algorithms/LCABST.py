228c5534260fc9923fa8e55f5453a6caa8feab44
Thomas Lopez <vondirath@gmail.com>
2017-01-17 21:19:19 -0700

    Add files via upload

--- algorithms/LCABST.py
+++ algorithms/LCABST.py
@@ -1,22 +1,24 @@
-class Node(object):
+class BSTNode(object):
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
 
 def question4(root, n1, n2):
-    if root is None or root.value == n1 or root.value == n2:
-        return root.value
     if root.value > n1 and root.value > n2:
         return question4(root.left, n1, n2)
     if root.value < n1 and root.value < n2:
         return question4(root.right, n1, n2)
     return root.value
 
+root = Node(20)
+root.left = Node(8)
+root.right = Node(22)
+root.left.left = Node(4)
+root.left.right = Node(12)
+root.left.right.left = Node(10)
+root.left.right.right = Node(14)
 
-root = Node(0)
-root.right = Node(4)
-root.right.left = Node(1)
-root.right.right = Node(2)
-
-print question4(root, 4, 2)
+print question4(root, 10, 14)
+print question4(root, 14, 8)
+print question4(root, 10, 22)
